46a30a606138fbc641c76f8b7c3ad2cf31ac284d
Mino1289 <kretzvictor@orange.fr>
2021-11-05 16:49:04 +0100

    [kandinsky/font] Simplify including code_points

--- kandinsky/fonts/rasterizer.c
+++ kandinsky/fonts/rasterizer.c
@@ -18,11 +18,7 @@
 #ifdef ISBUILDINGFLASHER
 #include "code_points.h"
 #else
-#ifdef HAS_READER
 #include "code_points_latex.h"
-#else
-#include "code_points.h"
-#endif
 #endif
 #include "../../ion/src/external/lz4/lz4hc.h"
 
